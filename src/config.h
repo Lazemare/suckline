2952174ee2a75f449f119fac12279bc757828b15
Lazemare <zjt033@gmail.com>
2020-07-16 13:35:35 +0800

    Rewrote header files

--- src/config.h
+++ src/config.h
@@ -6,31 +6,28 @@
  * http://www.wtfpl.net/ for more details. */
 
 /* Configurations */
-struct CONFIG config = {
-	.segment_number      = 6,       /* number of segments */
-	.directory_mode      = FIRST,   /* FIRST, LAST, FULL */
-	.prompt_symbol       = " $ ",
-	.prompt_symbol_err   = " 🤔 ",
-	.color_bg_repo_dirty = YEL,
 
-	.segment_type[0]     = USER,    /* Segment type:  */
-	.segment_type[1]     = CONDA,   /* USER, CONDA, PATH, */
-	.segment_type[2]     = PATH,    /* GIT,  SYMBO, ERRNO */
-	.segment_type[3]     = GIT,
-	.segment_type[4]     = SYMBO,
-	.segment_type[5]     = ERRNO,
+/*
+ * Segment types:
+ * USER, CONDA, PATH,
+ * GIT,  SYMBO, ERRNO
+ *
+ * Colors:
+ * BLK, RED, GRN, CYN,
+ * YEL, BLU, MAG, WHT
+ */
 
-	.color_fg[0]         = BLK,     /* Colors: */
-	.color_fg[1]         = WHT,     /* BLK, RED, GRN, CYN, */
-	.color_fg[2]         = WHT,     /* YEL, BLU, MAG, WHT  */
-	.color_fg[3]         = BLK,
-	.color_fg[4]         = WHT,
-	.color_fg[5]         = WHT,
+#define SEGMENT_NUMBER      6
+#define DIRECTORY_MODE      FIRST
+#define PROMPT_SYMBOL       " $ "
+#define PROMPT_SYMBOL_ERROR " 🤔 "
+#define REPO_DIRTY_BG       YEL
 
-	.color_bg[0]         = YEL,
-	.color_bg[1]         = MAG,
-	.color_bg[2]         = BLU,
-	.color_bg[3]         = CYN,
-	.color_bg[4]         = BLK,
-	.color_bg[5]         = RED,
+static struct SEGMENT segment[] = {
+	{USER,  BLK, YEL},
+	{CONDA, WHT, MAG},
+	{PATH,  WHT, BLU},
+	{GIT,   BLK, CYN},
+	{SYMBO, WHT, BLK},
+	{ERRNO, WHT, RED},
 };
