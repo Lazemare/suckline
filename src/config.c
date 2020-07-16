acfe331701344051ac6a12ccf9aacc79d0e0c864
Lazemare <zjt033@gmail.com>
2020-07-16 13:30:54 +0800

    Rewrote header files

--- src/config.c
+++ src/config.c
@@ -1,62 +1,57 @@
-#define _GNU_SOURCE
+/* This file is part of the suckline program.
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details. */
+
+/* This prompt sucks! */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include "suckline.h"
+#include "config.h"
+#include "color.h"
 
-/* Convert configurations into coresponding strings and arrays. */
-void conv_conf(struct CONFIG config, struct PROMPT *prompt)
+int main(int argc, char *argv[])
 {
-	int i = 0;
-	int git_info = 0;
-
-	for (i = 0; i < prompt->segnum; i++) {
-		/* strings */
-		switch (config.segment_type[i]) {
-		case 1:
-			get_username(prompt->string[i]);
-			break;
-		case 2:
-			my_getcwd(prompt->string[i],MAXLPS,config.directory_mode);
-			break;
-		case 3:
-			if (!prompt->errno) {
-				strcat(prompt->string[i],config.prompt_symbol);
-			} else {
-				strcat(prompt->string[i],config.prompt_symbol_err);
-			}
-			break;
-		case 4:
-			/* only print something when errno != 0 */
-			if (prompt->errno) {
-				sprintf(prompt->string[i]," %d ",prompt->errno);
-			}
-			break;
-		case 5:
-			get_conda_path(prompt->string[i]);
-			break;
-		case 6:
-			git_info = git(prompt->string[i]);
-			switch (git_info) {
-			case 1:
-				/* if not a repo, purge this segment */
-				break;
-			case 2:
-				/* git dir, give a warning */
-				config.color_bg[i] = config.color_bg_repo_dirty;
-				break;
-			case 3:
-				/* repo dirty */
-				config.color_bg[i] = config.color_bg_repo_dirty;
-				break;
-			}
-			break;
-		default:
-			memset(prompt->string[i], 0, MAXLPS);
-		}
-		/* colors */
-		prompt->color_fg[i] = config.color_fg[i];
-		prompt->color_bg[i] = config.color_bg[i];
+	/* Load configurations. */
+	struct PROMPT prompt;
+	prompt.errno = 0;
+	prompt.segnum = SEGMENT_NUMBER;
+	prompt.color_fg = (int *) malloc(SEGMENT_NUMBER*sizeof(int));
+	prompt.color_bg = (int *) malloc(SEGMENT_NUMBER*sizeof(int));
+	prompt.string = (char **) malloc(prompt.segnum*sizeof(char *));
+	for (int i = 0; i < prompt.segnum; i++) {
+		prompt.string[i] = (char *) malloc(MAXLPS*sizeof(char));
+		memset(prompt.string[i], 0, MAXLPS*sizeof(char));
 	}
+	prompt.buff = (char *) malloc(SEGMENT_NUMBER*MAXLPS*sizeof(char));
+	memset(prompt.buff, 0, SEGMENT_NUMBER*MAXLPS*sizeof(char));
+
+	if (argc > 1) {
+		if (strcmp(argv[1],"bash") == 0) {
+			prompt.shell = 0;
+		} else {
+			prompt.shell = 1;
+		}
+	} else {
+		prompt.shell = 1;
+	}
+	if (argc > 2) {
+		prompt.errno = atoi(argv[2]);
+	}
+	conv_conf(&prompt);
+	make_segment(&prompt);
+	write(1,prompt.buff,strlen(prompt.buff));
+
+	for (int i = 0; i < prompt.segnum; i++) {
+		free(prompt.string[i]);
+	}
+	free(prompt.color_fg);
+	free(prompt.color_bg);
+	free(prompt.string);
+	free(prompt.buff);
 }
