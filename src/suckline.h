e039baf5c085299030662364791a6ee4596a3d5c
Lazemare <zjt033@gmail.com>
2020-07-16 13:37:11 +0800

    Rewrote header files

--- src/suckline.h
+++ src/suckline.h
@@ -1,48 +1,43 @@
-/* prompt length */
-#define MAXLEN 10000 /* Big enough I think */
-#define MAXSEG 10
-#define MAXLPS 1000
+/* This file is part of the suckline program.
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details. */
+
+/* segment length */
+#define MAXLPS 1000 /* Big enough I think */
 
 /* segment name */
-#define USER  1
-#define PATH  2
-#define SYMBO 3
-#define ERRNO 4
-#define CONDA 5
-#define GIT   6
+#define USER   1
+#define PATH   2
+#define SYMBO  3
+#define ERRNO  4
+#define CONDA  5
+#define GIT    6
 /* segment color */
-#define BLK   0
-#define RED   1
-#define GRN   2
-#define YEL   3
-#define BLU   4
-#define MAG   5
-#define CYN   6
-#define WHT   7
+#define BLK    0
+#define RED    1
+#define GRN    2
+#define YEL    3
+#define BLU    4
+#define MAG    5
+#define CYN    6
+#define WHT    7
 /* directory mode */
-#define FULL  0
-#define FIRST 1
-#define LAST  2
+#define FULL   0
+#define FIRST  1
+#define LAST   2
 
 /* structures */
 
-struct CONFIG {
-	/* Number of segments */
-	int   segment_number;
+struct SEGMENT {
 	/* Type of each segment */
-	int   segment_type[MAXSEG];
+	int    segment_type;
 	/* foreground color number of each segment */
-	int   color_fg[MAXSEG];
+	int    color_fg;
 	/* background color number of each segment */
-	int   color_bg[MAXSEG];
-	/* background color when repo is dirty */
-	int   color_bg_repo_dirty;
-	/* directory showing mode */
-	int   directory_mode;
-	/* prompt symbol */
-	char *prompt_symbol;
-	/* prompt symbol when errno != 0 */
-	char *prompt_symbol_err;
+	int    color_bg;
 };
 
 struct PROMPT {
@@ -55,23 +50,23 @@ struct PROMPT {
 	/* strings of each segment */
 	char **string;
 	/* foreground color of each segment  */
-	int    color_fg[MAXSEG];
+	int    *color_fg;
 	/* background color of each segment  */
-	int    color_bg[MAXSEG];
+	int    *color_bg;
 	/* Output string */
 	char   *buff;
 };
 
 /* functions */
 
-void get_username(char *str);
-
 void my_getcwd(char *buff, int len, int mode);
 
-void conv_conf(struct CONFIG config, struct PROMPT *prompt);
+void conv_conf(struct PROMPT *prompt);
 
 void make_segment(struct PROMPT *prompt);
 
 void get_conda_path(char *buff);
+
+void get_username(char *str);
 
 int git(char *str);
